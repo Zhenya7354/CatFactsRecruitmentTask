ba5f9b7e2412179b4a1c1832f666ab4b7c73022d
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:22:45 +0200

    feat(Services): add CatFactApiProvider to interact with Cat Fact API

---
+++ CatFactsApp/Services/CatFactApiProvider.cs
@@ -0,0 +1,29 @@
+﻿using CatFactsApp.Configurations;
+using CatFactsApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace CatFactsApp.Services;
+
+public class CatFactApiProvider(
+    HttpClient httpClient,
+    IOptions<CatFactApiOptions> options,
+    IFileService fileService) : ICatFactApiProvider
+{
+    private readonly string _apiUrl = options.Value.BaseUrl + options.Value.Endpoint;
+    public async Task<CatFactApiResponse> GetCatFactAsync(CancellationToken cancellationToken)
+    {
+        var response = await httpClient.GetFromJsonAsync<CatFactApiResponse>(_apiUrl, cancellationToken);
+        return response ?? throw new InvalidOperationException("Failed to retrieve cat fact");
+    }
+
+    public async Task SaveCatFactToFileAsync(CatFactApiResponse fact, CancellationToken cancellationToken)
+    {
+        await fileService.SaveToFileAsync(fact, cancellationToken);
+    }
+}
+
+public interface ICatFactApiProvider
+{
+    Task<CatFactApiResponse> GetCatFactAsync(CancellationToken cancellationToken);
+    Task SaveCatFactToFileAsync(CatFactApiResponse fact, CancellationToken cancellationToken);
+}
