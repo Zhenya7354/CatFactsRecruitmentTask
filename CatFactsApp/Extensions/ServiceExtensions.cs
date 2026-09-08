28a69500539896f11737df49011ec5c0822c1eca
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:18:30 +0200

    feat(Extensions): add ServiceExtensions for DI registration

---
+++ CatFactsApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CatFactsApp.Services;
+
+namespace CatFactsApp.Extensions;
+
+public static class ServiceExtensions
+{
+    public static WebApplicationBuilder AddAppServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<ICatFactApiProvider, CatFactApiProvider>();
+        builder.Services.AddScoped<IFileService, FileService>();
+        builder.Services.AddHttpClient<ICatFactApiProvider, CatFactApiProvider>();
+        return builder;
+    }
+}
