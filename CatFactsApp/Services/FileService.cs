200c40879e177937aa917720171539a93dd3c821
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:24:07 +0200

    feat(Services): add FileService to save cat facts to local file

---
+++ CatFactsApp/Services/FileService.cs
@@ -0,0 +1,23 @@
+﻿using CatFactsApp.Models;
+using System.Text.Json;
+
+namespace CatFactsApp.Services;
+
+public class FileService : IFileService
+{
+    private readonly string _path = "CatFacts.txt";
+
+    public async Task SaveToFileAsync(CatFactApiResponse fact, CancellationToken cancellationToken)
+    {
+        if (fact is null)
+        {
+            return;
+        }
+        string factString = "Fact: " + fact.Fact + " | Length: " + fact.Length;
+        await File.AppendAllLinesAsync(_path, [factString], cancellationToken);
+    }
+}
+    public interface IFileService
+    {
+        public Task SaveToFileAsync(CatFactApiResponse fact, CancellationToken cancellationToken);
+    }
