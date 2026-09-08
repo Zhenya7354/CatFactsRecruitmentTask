25b61422743c6f035d9285b60fb7a2c75e64441c
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:28:37 +0200

    feat(Endpoints): add CatFactsEndpoints to get cat facts and save them into local file

---
+++ CatFactsApp/Endpoints/CatFactsEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using CatFactsApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace CatFactsApp.Endpoints;
+
+public static class CatFactsEndpoints
+{
+    public static WebApplication MapCatFactsEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("/catfacts");
+        group.MapGet("/", async ([FromServices] ICatFactApiProvider catFactApiProvider, CancellationToken cancellationToken) =>
+        {
+            var catFact = await catFactApiProvider.GetCatFactAsync(cancellationToken);
+            await catFactApiProvider.SaveCatFactToFileAsync(catFact, cancellationToken);
+            return Results.Ok(catFact);
+        });
+        return app;
+    }
+}
