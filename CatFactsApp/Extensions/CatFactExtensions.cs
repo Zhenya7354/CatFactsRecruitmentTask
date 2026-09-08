23636976115136eae1a08eca2e8795b5fcf4e896
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:20:25 +0200

    feat(Extensions): add CatFactExtensions for CatFactApiOptions IOptions registration

---
+++ CatFactsApp/Extensions/CatFactExtensions.cs
@@ -0,0 +1,12 @@
+﻿using CatFactsApp.Configurations;
+
+namespace CatFactsApp.Extensions;
+
+public static class CatFactExtensions
+{
+    public static WebApplicationBuilder AddCatFactConfigurations(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<CatFactApiOptions>(builder.Configuration.GetSection("CatFactApi"));
+        return builder;
+    }
+}
