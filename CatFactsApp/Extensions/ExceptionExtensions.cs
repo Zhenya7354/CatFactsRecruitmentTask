2ae3c702fb7f7ba2036368e8eae5d75aacf3a6fb
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-08 21:27:12 +0200

    feat(Extensions): add ExceptionExtension to register GlobalExceptionHandler in DI

---
+++ CatFactsApp/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CatFactsApp.Exceptions.ExceptionHandlers;
+
+namespace CatFactsApp.Extensions;
+
+public static class ExceptionExtensions
+{
+    public static WebApplicationBuilder AddExceptionHandlers(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+        builder.Services.AddProblemDetails();
+        return builder;
+    })
+}
