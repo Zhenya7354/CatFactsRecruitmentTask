8dc477c00fa159582384b45917d12fdb6f1c6d7f
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-10 21:00:53 +0200

    reafactor(Services): Rename CatFactApiProvider to CatFactClient

---
+++ CatFactsApp/Services/CatFactClient.cs
@@ -0,0 +1,27 @@
+﻿using CatFactsApp.Configurations;
+using CatFactsApp.Models;
+using Microsoft.Extensions.Options;
+
+namespace CatFactsApp.Services;
+
+public class CatFactClient(
+    HttpClient httpClient,
+    IOptions<CatFactApiOptions> options) : ICatFactClient
+{
+    private readonly string _apiUrl = options.Value.BaseUrl + options.Value.Endpoint;
+    public async Task<CatFact> GetCatFactAsync(CancellationToken cancellationToken)
+    {
+        var response = await httpClient.GetFromJsonAsync<CatFactApiResponse>(_apiUrl, cancellationToken);
+        if (response is null)
+        {
+            throw new InvalidOperationException("Cat fact API returned an empty response.");
+        }
+        return new CatFact(response.Fact, response.Length);
+    }
+
+}
+
+public interface ICatFactClient
+{
+    Task<CatFact> GetCatFactAsync(CancellationToken cancellationToken);
+}
