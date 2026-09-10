eda3834497b3a903e358eb6b1fc59547f20502a9
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-10 21:11:14 +0200

    feat(Results): add Result class to implement Result Pattern across Services

---
+++ CatFactsApp/Results/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace CatFactsApp.Results;
+
+public sealed class Result
+{
+    private Result(bool isSuccess, string? errorMessage)
+    {
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+    }   
+    public bool IsSuccess { get; }
+    public string? ErrorMessage { get; }
+    public static Result Success() => new(true, null);
+    public static Result Failure(string errorMessage) => new(false, errorMessage);
+}
