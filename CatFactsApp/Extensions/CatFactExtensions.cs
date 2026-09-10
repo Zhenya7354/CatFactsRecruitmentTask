c4f0ea1251ada6f7416b0b220373d4ca0b2091d0
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-10 21:27:02 +0200

    chore(CatFactExtensions): bindCatFactFileOptions to CatFactFile config section

--- CatFactsApp/Extensions/CatFactExtensions.cs
+++ CatFactsApp/Extensions/CatFactExtensions.cs
@@ -3,10 +3,13 @@
 namespace CatFactsApp.Extensions;
 
 public static class CatFactExtensions
-{
+{   
+    private const string CatFactApiSectionName = "CatFactApi";
+    private const string CatFactFileSectionName = "CatFactFile";
     public static WebApplicationBuilder AddCatFactConfigurations(this WebApplicationBuilder builder)
     {
-        builder.Services.Configure<CatFactApiOptions>(builder.Configuration.GetSection("CatFactApi"));
+        builder.Services.Configure<CatFactApiOptions>(builder.Configuration.GetSection(CatFactApiSectionName));
+        builder.Services.Configure<CatFactFileOptions>(builder.Configuration.GetSection(CatFactFileSectionName));
         return builder;
     }
 }
