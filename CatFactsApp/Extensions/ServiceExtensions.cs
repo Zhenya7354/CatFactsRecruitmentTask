dac10709a12f227f3a4753a47654a71f885053ce
Zhenya7354 <154931489+Zhenya7354@users.noreply.github.com>
2026-09-10 21:27:32 +0200

    chore(ServiceExtensions): register CatFactService in DI

--- CatFactsApp/Extensions/ServiceExtensions.cs
+++ CatFactsApp/Extensions/ServiceExtensions.cs
@@ -6,9 +6,9 @@ public static class ServiceExtensions
 {
     public static WebApplicationBuilder AddAppServices(this WebApplicationBuilder builder)
     {
-        builder.Services.AddScoped<ICatFactApiProvider, CatFactApiProvider>();
         builder.Services.AddScoped<IFileService, FileService>();
-        builder.Services.AddHttpClient<ICatFactApiProvider, CatFactApiProvider>();
+        builder.Services.AddScoped<ICatFactService, CatFactService>();
+        builder.Services.AddHttpClient<ICatFactClient, CatFactClient>();
         return builder;
     }
 }
